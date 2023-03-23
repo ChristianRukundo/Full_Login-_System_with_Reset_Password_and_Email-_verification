646dfd6d0bceffd5f2f33c308a431f249d348339
ChristianMrCoder <siboruremarukundoc@gmail.com>
2023-03-23 07:17:11 +0200

    initial commit

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "<secret>",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb://127.0.0.1/LoginSystem"
+}
